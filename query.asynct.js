ae8877bce43cecabcf65f0a68a4691d6dd875ad7
Dominic Tarr <dominic.tarr@gmail.com>
2010-12-16 18:56:31 +1300

    update

--- query.asynct.js
+++ query.asynct.js
@@ -1,4 +1,5 @@
 
+console.log("##################################################################################")
 
 var query = require('query')
   , should = require('should')
--- query.js
+++ query.js
@@ -1,6 +1,6 @@
 //query
 
-  inspect = require('inspect')
+inspect = require('inspect')
 
 module.exports = query
 
@@ -32,21 +32,16 @@ function all (object,func){
   if(func === undefined)
     return object
   func = checker(func)
-  //console.log("searching for :" + inspect(func))
-  //console.log("   in :" + inspect(object))
 
   var l = newObj(object),n,push = (object instanceof Array)
   for(key in object){
       var value = object[key]
           if(func(value,key)) {
-      //console.log('add:' + inspect(value) + " ?")
 
       if(push) {l.push(value)} else {l[key] = value}//perserve keys if it's an object.
 
     }
   }
-  //console.log("   push? :" + push)
-  //console.log("   found :" + inspect(l))
   
   return l
 }
@@ -119,31 +114,23 @@ function checker(func){
   if('function' == typeof func)
     return func
 
-//  //console.log(typeof func, inspect(func))  
-
   if('object' != typeof func) {
     var e
     if('string' === typeof func && (e = isRegExp.exec(func))){
       //check if string is a regular expression
-      r = new RegExp(e[1],e[2])
+      var r = new RegExp(e[1],e[2])
       return function (other){
       var m = r.exec(other); 
- //     console.log(r.toString() + "(" + inspect(other) + ') = ' + (m && m[0]))
-
       return m && m[0]
       }
     } else {
       
-      x = function (other){
-      console.log(inspect(other) + " == " + func + ' = ' + (other == func))
-      return other == func
-      }
+      x = function (other){ return other == func }
       x.target = func // information for debuging, 
       return x
     }
   }
   return function (other){
-      
 
     for (i in func){
       key = checker(i)  //currently rebuilding checkers each iteration, bad.
@@ -152,18 +139,14 @@ function checker(func){
       var match = false
       for(j in other){
         if(key(j)){
-//          console.log("" + key.target + "(" + j + ") matched")
           if(value(other[j]))
             match = true
         }
       }
       if (!match){
-        //console.log("no match")
         return undefined
       }
     }
- //   console.log("matched object :" + inspect(other) + " to query " + inspect(func) )
     return other
   }
-//  throw new Error ("not implemented yet")    
 }
